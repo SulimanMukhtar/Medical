eb501b796b5744bce5d13f7bcfbdfd2a8d12fd6c
qamer-del <55939365+qamer-del@users.noreply.github.com>
2021-09-08 01:33:54 -0700

    Some Edits

---
+++ Medical/resources/views/includes/footer.blade.php
@@ -0,0 +1,17 @@
+{{-- <section class="footer"> 
+    <div class="copyright text-center justify-content-end">
+      copyright &copy; 2021 <span>Portifiolo</span> .Inc By Designer : <strong>Qamer Ibrahim</strong>
+    </div>
+</section> --}}
+
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="#"> MEDICA</a>
+    </div>
+    <!-- Copyright -->
+  
+  </footer>
+  <!-- Footer -->
