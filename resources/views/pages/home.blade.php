a8a25a1fd6079c7a5882a9897babb4b80cf79bd8
Qamer <gameribrahim9@gmail.com>
2021-12-10 05:31:20 -0800

    lskqks

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -38,9 +38,43 @@
         <div class="main-body">
             <h1>WE PROVIDE TOP MEDICAL SERVICE </h1>
             <P class="lead">MORE THAN 30 PROFESSIONAL</P>
-            <button class="Appiontment-btn"> JOIN TO US</button>
+            <button data-target="#exampleModal" data-toggle="modal" class="Appiontment-btn" > JOIN TO US</button>
         </div>
     </div>
+    <!-- stsrt join us modal--------------------------------------------------------- -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog  modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Medica</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+         <p class="lead joints">Welcome To Medica Community , Please Choose What Represent Your Service. </p>
+         <div class="container">
+  <div class="row joinbt">
+    <div class="col-sm">
+     <button class ="join" >Pharmacy</button>
+    </div>
+    <div class="col-sm">
+    <button class ="join" >Doctor</button>
+    </div>
+    <div class="col-sm">
+    <button class ="join" >Laboratory</button>
+    </div>
+  </div>
+</div>
+
+      </div>
+      <div class="modal-footer">
+       
+      </div>
+    </div>
+  </div>
+</div>
+    <!-- end join us modal--------------------------------------------------------- -->
     @include('includes.footer')
     <script src="js/jquery-3.6.0.min.js"></script>
     <script src="js//bootstrap.min.js"></script>
